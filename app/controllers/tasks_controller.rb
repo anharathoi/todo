dc0647e77d11df9c1c6a6bfc55033abc6b6ed8e9
Athoi <athoi@Athois-MacBook-Pro.local>
2018-10-28 21:00:43 +1100

    Implemented delete and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,16 @@ class TasksController < ApplicationController
      redirect_to(root_path)   
   end
 
+  def edit
+    @task = Task.find(params[:id]) 
+  end
+
+  def update
+    @task = Task.find(params[:id]) 
+    @task.update(task_params)
+    redirect_to(root_path)   
+  end
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # resources :tasks
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to:'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to:'tasks#update'
   delete 'tasks/:id', to:'tasks#destroy', as: 'task'
   get 'tasks/:id', to: 'tasks#show'
   post 'tasks', to: 'tasks#create'
